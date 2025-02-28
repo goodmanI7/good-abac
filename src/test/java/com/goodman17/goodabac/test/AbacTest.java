b23ec57a233d50757b454b75a2e62db4174daeb8
李仁豪 <anotherme@aliyun.com>
2025-02-28 19:31:18 +0800

    update

---
+++ src/test/java/com/goodman17/goodabac/test/AbacTest.java
@@ -0,0 +1,17 @@
+package com.goodman17.goodabac.test;
+
+import org.casbin.jcasbin.main.Enforcer;
+import org.junit.Test;
+
+/**
+ * @author lirenhao
+ * date: 2025/2/27 19:17
+ */
+public class AbacTest {
+
+    @Test
+    public void testEval() {
+        Enforcer e = new Enforcer("examples/abac_rule_model.conf");
+//        e.addPolicy()
+    }
+}
